ea5d830687a6d3dc4a9191099950fc9b16108dcd
ivan <252127196@qq.com>
2023-12-24 18:50:57 +0800

    init: 增加finance模块

---
+++ lego-cloud/lego-finance/lego-finance-server/src/main/java/com/shenzhen/dai/LegoFinanceApplication.java
@@ -0,0 +1,11 @@
+package com.shenzhen.dai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoFinanceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LegoFinanceApplication.class, args);
+    }
+}

048ef2074a1fdc7662fdad25eadca96b1f5e734a
ivan <252127196@qq.com>
2023-12-25 07:14:16 +0800

    feature: 服务注册到nacos

--- lego-cloud/lego-finance/lego-finance-server/src/main/java/com/shenzhen/dai/LegoFinanceApplication.java
+++ lego-cloud/lego-finance/lego-finance-server/src/main/java/com/shenzhen/dai/LegoFinanceApplication.java
@@ -2,9 +2,11 @@ package com.shenzhen.dai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient // 开启服务发现
 @SpringBootApplication
 public class LegoFinanceApplication {
 

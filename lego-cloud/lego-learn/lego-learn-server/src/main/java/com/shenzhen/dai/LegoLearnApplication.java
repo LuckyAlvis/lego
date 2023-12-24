9f745f9c75532ed1d72cec8a0acd30a8b4c28ef2
ivan <252127196@qq.com>
2023-12-24 19:00:05 +0800

    fix: learn模块增加启动类和必要配置

---
+++ lego-cloud/lego-learn/lego-learn-server/src/main/java/com/shenzhen/dai/LegoLearnApplication.java
@@ -0,0 +1,11 @@
+package com.shenzhen.dai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoLearnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LegoLearnApplication.class, args);
+    }
+}

9195a65bf5c719c74bba72264288fd39090d5a51
ivan <252127196@qq.com>
2023-12-24 14:17:47 +0800

    feature: LegoBackendAdmin项目web初始化

---
+++ lego-backend-admin/src/main/java/com/shenzhen/dai/LegoBackendAdminServerApplication.java
@@ -0,0 +1,11 @@
+package com.shenzhen.dai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoBackendAdminServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LegoBackendAdminServerApplication.class, args);
+    }
+}

c40b6d56f5ff45b7e5ea22e26e7c2971d5d4f585
ivan <252127196@qq.com>
2023-12-25 08:00:30 +0800

    feature: 实现手动load balance

--- lego-cloud/lego-finance/lego-finance-server/src/main/java/com/shenzhen/dai/controller/PingController.java
+++ lego-cloud/lego-finance/lego-finance-server/src/main/java/com/shenzhen/dai/controller/PingController.java
@@ -1,23 +1,45 @@
 package com.shenzhen.dai.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Random;
+
+@Slf4j
 @RestController
 public class PingController {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/ping")
     public String ping() {
-        return "pong, this is finance server!";
+        String result = "pong, this is finance server!";
+        log.info(result);
+        return result;
     }
 
     @GetMapping("/pingLearn")
-    public String b() {
+    public String pingLearn() {
         return restTemplate.getForObject("http://localhost:8082/ping", String.class).toString();
+    }
+
+    @GetMapping("/loadBalanceManually")
+    public String loadBalanceManually() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("lego-learn-server");
+        Integer index = new Random().nextInt(instances.size());
+        ServiceInstance serviceInstance = instances.get(index);
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/ping";
+        System.out.println("ping " + url);
+        return restTemplate.getForObject(url, String.class).toString();
     }
 }
--- lego-cloud/lego-learn/lego-learn-server/src/main/java/com/shenzhen/dai/controller/PingController.java
+++ lego-cloud/lego-learn/lego-learn-server/src/main/java/com/shenzhen/dai/controller/PingController.java
@@ -1,23 +1,45 @@
 package com.shenzhen.dai.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Random;
+
+@Slf4j
 @RestController
 public class PingController {
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/ping")
     public String ping() {
-        return "pong, this is learn server!";
+        String result = "pong, this is learn server!";
+        log.info(result);
+        return result;
     }
 
     @GetMapping("/pingFinance")
     public String pingFinance() {
         return restTemplate.getForObject("http://localhost:8084/ping", String.class).toString();
+    }
+
+    @GetMapping("/loadBalanceManually")
+    public String loadBalanceManually() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("lego-finance-server");
+        Integer index = new Random().nextInt(instances.size());
+        ServiceInstance serviceInstance = instances.get(index);
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/ping";
+        System.out.println("ping " + url);
+        return restTemplate.getForObject(url, String.class).toString();
     }
 }

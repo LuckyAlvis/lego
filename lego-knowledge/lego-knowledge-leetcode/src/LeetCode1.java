08effd22c9a64a51ce2d9d3075aea49d4f9b0036
ivan <252127196@qq.com>
2024-10-03 14:50:21 +0800

    knowledge: LeetCode1

---
+++ lego-knowledge/lego-knowledge-leetcode/src/LeetCode1.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description:
+ * @author: daiyifan
+ * @create: 2024-10-03 14:33
+ */
+public class LeetCode1 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{3, 3};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashTable = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashTable.containsKey(target - nums[i])) {
+                return new int[]{hashTable.get(target - nums[i]), i};
+            } else {
+                hashTable.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+}
